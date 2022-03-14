53b437917be49c50f59f81a9de36e986e5fd32fc
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-14 17:14:18 -0300

    skeleton files

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function operate(operator, inputNumOne, inputNumTwo) {
+	
+}
+
+function addition(inputNumOne, inputNumTwo) {
+		
+}
+
+function subtraction(inputNumOne, inputNumTwo) {
+	
+}
+
+function multiplication(inputNumOne, inputNumTwo){
+	
+}
+
+function division(inputNumOne, inputNumTwo) {
+	
+}
