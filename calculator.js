e84896a337bb71dad00d1308c86cce544c533f5f
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-16 12:50:40 -0300

    Addition function added and works correctly

--- calculator.js
+++ calculator.js
@@ -1,21 +1,29 @@
 function operate(num, operator) {
-	total
+	if (operator === 'plus') {
+		total = addition(num);
+		num[0] = total;
+		console.log(total);
+	}
+}
+
+function addition(num) {
+	return num[0] + num[1];
 }
 
 const numButtons = document.querySelectorAll(".number");
 const operButtons = document.querySelectorAll(".operator");
 let num = [null];
 let oper;
-let total;
+let total = 0;
 
 numButtons.forEach(button => {
 	button.addEventListener('click', function() {
 		if (num[0] === null) {
-			num[0] = button.value;
+			num[0] = parseInt(button.value);
 			console.log(num);
 		}
 		else {
-			num[1] = button.value;
+			num[1] = parseInt(button.value);
 			console.log(num);
 		}
 	});
@@ -25,6 +33,10 @@ operButtons.forEach(button => {
 	button.addEventListener('click', function() {
 		oper = button.value;
 		console.log(oper);
+		
+		if (num.length === 2) {
+			operate(num, oper);
+		}
 	});
 });
 
