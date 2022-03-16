05342ea9b955163d70725f1ee59ffa8ac4d65bf7
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-16 13:59:08 -0300

    Added subtraction function, fixed operator logic can now successfully add multiple operations in one session

--- calculator.js
+++ calculator.js
@@ -1,28 +1,46 @@
 function operate(num, operator) {
-	if (operator === 'plus') {
+	// Addition function returns the sum of the two numbers in num array. Num[0] stores total to be used next operation
+	if (operator[0] === 'plus') {
+		
 		total = addition(num);
+		num[0] = total;
+		console.log(total);
+	}
+	
+	// Subtraction
+	if (operator[0] === 'minus') {
+		
+		total = subtraction(num);
 		num[0] = total;
 		console.log(total);
 	}
 }
 
 function addition(num) {
+	
 	return num[0] + num[1];
+}
+
+function subtraction(num) {
+	
+	return num[0] - num[1];
 }
 
 const numButtons = document.querySelectorAll(".number");
 const operButtons = document.querySelectorAll(".operator");
 let num = [null];
-let oper;
+let oper = [null];
 let total = 0;
 
 numButtons.forEach(button => {
 	button.addEventListener('click', function() {
 		if (num[0] === null) {
+			
 			num[0] = parseInt(button.value);
 			console.log(num);
 		}
 		else {
+			
 			num[1] = parseInt(button.value);
 			console.log(num);
 		}
@@ -31,11 +49,21 @@ numButtons.forEach(button => {
 
 operButtons.forEach(button => {
 	button.addEventListener('click', function() {
-		oper = button.value;
-		console.log(oper);
+		if (oper[0] === null) {
+			
+			oper[0] = button.value;
+			console.log(oper);
+		}
+		else {
+			
+			oper[1] = button.value;
+			console.log(oper);
+		}
 		
-		if (num.length === 2) {
+		if (num.length === 2 && oper.length === 2) {
+			
 			operate(num, oper);
+			oper.reverse();
 		}
 	});
 });
